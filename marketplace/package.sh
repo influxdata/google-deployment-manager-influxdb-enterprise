e8e5703eb7ea7c015a9b8b8d395c9df703493934
Gunnar Aasen <gunnar.r.aasen@gmail.com>
2018-10-01 01:04:12 -0400

    Clean up remaining issues for submission

--- marketplace/package.sh
+++ marketplace/package.sh
@@ -4,11 +4,13 @@ set -euxo pipefail
 
 rm -f influxdb-enterprise-byol.zip
 tmp="$(mktemp -d -t influxdb-enterprise-byol)/"
+cwd=$PWD
 
 cp influxdb-enterprise-byol.jinja $tmp
 cp influxdb-enterprise-byol.jinja.schema $tmp
 cp influxdb-enterprise-byol.jinja.display $tmp
 cp test_config.yaml $tmp
+cp c2d_deployment_configuration.json $tmp
 cp -r resources $tmp
 
 cp ../simple/data-node.jinja $tmp
@@ -17,10 +19,14 @@ cp ../simple/meta-node.jinja $tmp
 cp ../simple/meta-node.jinja.schema $tmp
 cp ../simple/runtime-config.jinja $tmp
 cp ../simple/runtime-config.jinja.schema $tmp
+cp ../simple/network.jinja $tmp
+cp ../simple/network.jinja.schema $tmp
 cp ../simple/setup-common.sh $tmp
 cp ../simple/setup-meta.sh $tmp
 cp ../simple/setup-data.sh $tmp
 cp ../simple/password.py $tmp
 
-zip -r -X influxdb-enterprise-byol.zip $tmp
+cd $tmp
+zip -r -X $cwd/influxdb-enterprise-byol.zip ./*
+cd -
 rm -rf $tmp
